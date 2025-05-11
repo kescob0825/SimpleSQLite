a971dcd2d9b90c3ab78e66e8519b1d53c316c9d8
kescob0825 <escobar.karl0825@gmail.com>
2025-05-11 09:34:50 -0700

    Refactored InputBuffer to the core package

---
+++ core/InputBuffer.h
@@ -0,0 +1,21 @@
+#ifndef INPUTBUFFER_H
+#define INPUTBUFFER_H
+
+#include <stdio.h>
+// This is a small wrapper class to store some state of the
+// InputBuffer in order to interact with a "getline()" function.
+typedef struct {
+    char* buffer;
+    size_t buffer_length;
+    ssize_t input_length;
+} InputBuffer;
+
+InputBuffer* new_input_buffer();
+
+void print_prompt();
+
+void read_input(InputBuffer* input_buffer);
+
+void close_input_buffer(InputBuffer* input_buffer);
+
+#endif //INPUTBUFFER_H
