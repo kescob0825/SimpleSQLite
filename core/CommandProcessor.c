7015c31e48485e7b48282579b78a003380a86cc8
kescob0825 <escobar.karl0825@gmail.com>
2025-05-11 09:55:08 -0700

    Exit MetaCommandAdded

--- core/CommandProcessor.c
+++ core/CommandProcessor.c
@@ -7,7 +7,7 @@
 MetaCommandType exec_meta_command(InputBuffer* input_buffer) {
     if (input_buffer->buffer[0] == '.') {
         if (strcmp(input_buffer->buffer, ".exit") == 0) {
-            exit(EXIT_SUCCESS);
+            return META_COMMAND_EXIT;
         } else {
             return META_COMMAND_UNRECOGNIZED;
         }
